34deafddbc4be035cf8b1a0d90e261b72682d033
musta902 <80974475+musta902@users.noreply.github.com>
2021-06-10 18:41:41 +0200

    added

--- EU5TestNGSelenium/src/test/java/com/cybertek/tests/HomeWorks/hw.java
+++ EU5TestNGSelenium/src/test/java/com/cybertek/tests/HomeWorks/hw.java
@@ -52,7 +52,29 @@ public class hw {
         }
 
 
+        WebDriver driver2 = WebDriverFactory.getDriver("chrome");
 
+        driver2.get("http://wikipedia.org");
+
+
+        WebElement searchWiki= driver2.findElement(By.xpath("//input[@id='searchInput']"));
+
+        searchWiki.sendKeys("selenium webdriver");
+
+        driver2.findElement(By.xpath("//button[@class='pure-button pure-button-primary-progressive']")).click();
+
+
+        driver2.findElement(By.xpath("(//div/a[@href])[4]")).click();
+
+        String expectedURL="https://en.wikipedia.org/wiki/Selenium_(software)";
+        System.out.println("driver2.getCurrentUrl() = " + driver2.getCurrentUrl());
+
+        if(expectedURL.equals(driver2.getCurrentUrl())){
+
+            System.out.println("PASS");
+        }else{
+            System.out.println("FAil");
+        }
 
 
 
--- EU5TestNGSelenium/src/test/java/com/cybertek/tests/HomeWorks/hwTc3.java
+++ EU5TestNGSelenium/src/test/java/com/cybertek/tests/HomeWorks/hwTc3.java
@@ -7,34 +7,43 @@ import org.openqa.selenium.WebElement;
 
 public class hwTc3 {
     public static void main(String[] args) {
+      //Go to wikipedia.org (Links to an external site.)
+       WebDriver driver=WebDriverFactory.getDriver("chrome");
+       driver.get("https://www.wikipedia.org");
 
-        WebDriver driver2 = WebDriverFactory.getDriver("chrome");
+       //enter search term `selenium webdriver`
+        WebElement searchBox = driver.findElement(By.xpath("//input[@id='searchInput']"));
+        searchBox.sendKeys("selenium webdriver");
 
-        driver2.get("http://wikipedia.org");
+        //click on search button
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
 
+        //click on search result `Selenium (software)`
+        driver.findElement(By.cssSelector("a[href$='Selenium_(software)']")).click();
 
-        WebElement searchWiki= driver2.findElement(By.xpath("//input[@id='searchInput']"));
-
-        searchWiki.sendKeys("selenium webdriver");
-
-        driver2.findElement(By.xpath("//button[@class='pure-button pure-button-primary-progressive']")).click();
-
-
-        driver2.findElement(By.xpath("(//div/a[@href])[4]")).click();
-
-        String expectedURL="Selenium_(software)";
-        System.out.println("driver2.getCurrentUrl() = " + driver2.getCurrentUrl());
-
-        if(expectedURL.equals(driver2.getCurrentUrl())){
-
+        //verify url ends with `Selenium_(software)'
+        String currentUrl = driver.getCurrentUrl();
+        System.out.println("currentUrl = " + currentUrl);
+        String expectedUrl = "Selenium_(software)";
+        if(currentUrl.endsWith(expectedUrl)){
             System.out.println("PASS");
+
+
         }else{
-            System.out.println("FAil");
+            System.out.println("FAIL");
         }
 
 
 
 
 
+        driver.quit();
     }
 }
+
+//Test case 3
+//Go to wikipedia.org (Links to an external site.)
+//enter search term `selenium webdriver`
+//click on search button
+//click on search result `Selenium (software)`
+//verify url ends with `Selenium_(software)'
