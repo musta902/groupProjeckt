22469058844aa0e7d14796fb8a1244f1af47664e
musta902 <80974475+musta902@users.noreply.github.com>
2021-06-10 19:01:53 +0200

    initial commit

---
+++ Mavenproject/src/test/java/halloTest.java
@@ -0,0 +1,6 @@
+public class halloTest {
+    public static void main(String[] args) {
+        System.out.println("hallo");
+    }
+
+}
---
+++ first-intellij-project/src/work1/hello.java
@@ -0,0 +1,8 @@
+package work1;
+
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("work1 started");
+    }
+}
